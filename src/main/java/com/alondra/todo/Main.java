6717bd40c610002d3cc1a510bf9be54e8e9d0a24
Alondra Faviola Reyes-Pagan <areyespagan@valenciacollege.edu>
2026-02-26 08:50:23 -0500

    TESTING

--- src/main/java/com/alondra/todo/Main.java
+++ src/main/java/com/alondra/todo/Main.java
@@ -7,6 +7,6 @@ public class Main {
 
         System.out.println("Task count: " + service.getTaskCount());
         System.out.println("Tasks: " + service.getTasks());
-        System.out.println("Demo build test!");
+        System.out.println("TESTING");
     }
 }
